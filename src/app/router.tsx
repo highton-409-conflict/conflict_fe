fe290e2e56bd54282c423f954c21cb5a4017843c
서지유 <jihoseo2008@gmail.com>
2026-02-21 21:15:40 +0900

    fix :: 아직 사용하지 않는 import 주석처리

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,5 @@
 import { LoginPage, SignupPage } from "@/pages"
-import { AuthGuard } from "@/shared/lib"
+// import { AuthGuard } from "@/shared/lib"
 import { createBrowserRouter } from "react-router"
 import { AppLayout } from "@/shared/layout/AppLayout"
 import { FeedPage } from "@/pages/feed"
