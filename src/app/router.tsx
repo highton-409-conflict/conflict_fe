d1f6230ebc2871a184f045a25b65541c18be466c
서지유 <jihoseo2008@gmail.com>
2026-02-21 21:13:35 +0900

    feat :: 레이아웃 분리

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,28 +2,19 @@ import { LoginPage, SignupPage } from "@/pages"
 import { AuthGuard } from "@/shared/lib"
 import { createBrowserRouter } from "react-router"
 import { AppLayout } from "@/shared/layout/AppLayout"
+import { FeedPage } from "@/pages/feed"
+import { AuthLayout } from "@/shared/layout/AuthLayout"
 
 export const router = createBrowserRouter([
     {
-        element: <AppLayout />,
+        element: <AuthLayout />,
         children: [
-            {
-                path: "/login",
-                element: <LoginPage />,
-            },
-            {
-                path: "/signup",
-                element: <SignupPage />,
-            },
-            {
-                element: <AuthGuard />,
-                children: [
-                    {
-                        path: "/",
-                        // element: <HomePage />,
-                    },
-                ],
-            },
+            { path: "/login", element: <LoginPage /> },
+            { path: "/signup", element: <SignupPage /> },
         ],
+    },
+    {
+        element: <AppLayout />,
+        children: [{ path: "/", element: <FeedPage /> }],
     },
 ])
